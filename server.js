8d3271e2d56a1352d6b984d3faeae707190334bf
Erick Bogarin <erick.bogarin@gmail.com>
2016-09-18 18:51:35 -0400

    remove unnecessary files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('mongodb://localhost/alurapic');
+require('./config/database')('mongodb://localhost/meanpic');
 
 http.createServer(app).listen(3000, function() {
     console.log('Servidor iniciado');
